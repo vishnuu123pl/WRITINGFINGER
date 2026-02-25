ed500c0f52e95b74dc456b442e5220c7fbee36d8
VISHNUUUUUUUU <137207623+vishnuu123pl@users.noreply.github.com>
2026-02-25 11:01:28 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,40 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Browse from "./pages/Browse";
+import ProductDetails from "./pages/ProductDetails";
+import Chat from "./pages/Chat";
+import Payment from "./pages/Payment";
+import Profile from "./pages/Profile";
+import PostItem from "./pages/PostItem";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/browse" element={<Browse />} />
+          <Route path="/item/:id" element={<ProductDetails />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/post" element={<PostItem />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
